0ad9b39653db2ca4845b1a13d06dd8e016479b14
Finn de Lange <78510191+FJTDL@users.noreply.github.com>
2022-04-01 10:30:08 +1300

    Adding all files
    Added every file for this version.
    NOTE: This version is currently in a mostly functional state. The purpose of this repo was to create a separate repo to house this version away from the main version.

---
+++ urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+
+from . import views
+
+app_name = 'calculator'
+
+urlpatterns = [
+  path('', views.home, name=''),
+  path('home', views.home, name='home'),
+  path('my_account', views.my_account, name='my_account'),
+  path('calculator', views.score, name='calculate'),
+  path('output', views.output, name='output'),
+  path('about', views.about, name='about'),
+  path('options', views.options, name='options'),
+  path('electric', views.electric, name='electric'),
+  path('provider', views.provider, name='provider'),
+  path('reduce_reuse_recycle', views.reduce_reuse_recycle, name='reduce_reuse_recycle'),
+  path('register', views.register, name='register'),
+  path('login', views.login_request, name='login'),
+  path('logout', views.logout_request, name='logout'),
+]
